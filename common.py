13f538743e99cee56aa50d9c6590aa149b1c5881
dexter <dexter2206@gmail.com>
2018-04-23 01:46:07 +0200

    Add previously omitted common module

---
+++ common.py
@@ -0,0 +1,20 @@
+"""Common classes used in aubergine."""
+import logging
+
+
+class Loggable:
+    """Base class for components supporting logging.
+
+    This base class was extracted in case we switch to another logging framework at some
+    point.
+    """
+
+    @property
+    def logger_name(self):
+        """Name of the logger used by this instance."""
+        return self.__class__.__name__
+
+    @property
+    def logger(self):
+        """Get logger used by this class."""
+        return logging.getLogger(self.logger_name)
