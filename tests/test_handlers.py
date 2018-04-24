82e4b31a0960b0f3be9ba584f3c3083a5ba5e75c
dexter <dexter2206@gmail.com>
2018-04-24 21:07:59 +0200

    Adapt handlers' test to fit refactored Extractor's structure

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -1,46 +1,30 @@
 """Test cases for request handlers."""
-import unittest
-from unittest import mock
-from aubergine.decoders import ParameterValidationError
+import pytest
+from aubergine import extractors
+from aubergine.extractors import ParameterValidationError
 from aubergine.handlers import RequestHandler
 
 
-class TestRequestHandler(unittest.TestCase):
-    """Test case for RequestHandler class."""
+@pytest.fixture
+def body_extractor(mocker):
+    return mocker.Mock()
 
-    body_dec = mock.Mock()
-    path_dec = mock.Mock()
-    get_dec = mock.Mock()
-    err_dec = mock.Mock()
+@pytest.fixture
+def path_extractor(mocker):
+    return mocker.Mock()
 
-    @classmethod
-    def setUpClass(cls):
-        """Set up test class."""
-        cls.err_dec.decode.side_effect = ParameterValidationError(['error msg1', 'error mesg2'])
-        cls.body_dec.decode.return_value = {'name': 'John', 'last_name': 'Doe'}
-        cls.get_dec.decode.return_value = 20
-        cls.path_dec.decode.return_value = 'johndoe'
+@pytest.fixture
+def query_extractor(mocker):
+    return mocker.Mock()
 
-    def setUp(self):
-        """Set up test."""
-        for _mock in (self.body_dec, self.path_dec, self.get_dec, self.err_dec):
-            _mock.reset_mock()
-
-    def test_get_parameter_dict(self):
-        """RequestHandler should correctly extract parameters using its decoders."""
-        param_decoders = {'uname': self.path_dec, 'limit': self.get_dec}
-        operation = mock.Mock()
-        req = mock.Mock()
-        kwargs = {'uname': '_johndoe_'}
-
-        handler = RequestHandler(operation, self.body_dec, param_decoders)
-        params = handler.get_parameter_dict(req, **kwargs)
-
-        exp_params = {
-            'uname': 'johndoe',
-            'limit': 20
-        }
-
-        self.assertEqual(exp_params, params)
-        self.path_dec.decode.assert_called_once_with(req, **kwargs)
-        self.get_dec.decode.assert_called_once_with(req, **kwargs)
+def test_handler_extracts_params(body_extractor, path_extractor, query_extractor, mocker):
+    """RequestHandler.get_parameter_dict should use its extractors and return extracted values."""
+    handler = RequestHandler(mocker.Mock(), body_extractor, [path_extractor, query_extractor])
+    req = mocker.Mock()
+    path_extractor.param_name = 'param1'
+    query_extractor.param_name = 'param2'
+    params = handler.get_parameter_dict(req, test=123, test2='test')
+    path_extractor.extract.assert_called_once_with(req, test=123,test2='test')
+    query_extractor.extract.assert_called_once_with(req, test=123,test2='test')
+    assert params == {'param1': path_extractor.extract.return_value,
+                      'param2': query_extractor.extract.return_value}
