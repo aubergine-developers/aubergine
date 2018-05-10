3fec1e6e108cbe7a8e6ca095bf6469bbc91808ab
Konrad Jałowiecki <dexter2206@gmail.com>
2018-05-11 00:18:15 +0200

    Switch from unittest to pytest in test_resource

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -1,30 +1,24 @@
 """Test cases for resource module."""
-import unittest
-from unittest import mock
+import pytest
 from aubergine.resource import Resource
 
 HTTP_METHODS = ['POST', 'GET', 'PUT', 'PATCH', 'OPTIONS', 'HEAD', 'DELETE']
 
+@pytest.mark.parametrize('method', HTTP_METHODS)
+def test_not_defined_methods(method, mocker):
+    handlers = {other_method: mocker.Mock() for other_method in HTTP_METHODS
+                if other_method != method}
+    res = Resource(handlers, 'some/path/')
+    with pytest.raises(AttributeError):
+        getattr(res, 'on_' + method.lower())(mocker.Mock(), mocker.Mock())
 
-class TestResource(unittest.TestCase):
-    """Test case for Resource class."""
-
-    def test_not_defined_methods(self):
-        """Request should not construct responders for methods not defined in handlers param."""
-        for method in HTTP_METHODS:
-            handlers = {other_method: mock.Mock() for other_method in HTTP_METHODS
-                        if other_method != method}
-            res = Resource(handlers, 'some/path/')
-            with self.subTest(method=method), self.assertRaises(AttributeError):
-                getattr(res, 'on_' + method.lower())(mock.Mock(), mock.Mock())
-
-    def test_dispatch_to_handler(self): # pylint: disable=no-self-use
-        """Request's responders should forward requests to correct handlers."""
-        handlers = {method: mock.Mock() for method in HTTP_METHODS}
-        resp = mock.Mock()
-        req = mock.Mock()
-        kwargs = {'arg1': 12, 'arg2': 'someargument'}
-        resource = Resource(handlers, '/some/path/')
-        for method in HTTP_METHODS:
-            getattr(resource, 'on_' + method.lower())(req, resp, **kwargs)
-            handlers[method].handle_request.assert_called_once_with(req, resp, **kwargs)
+def test_dispatch_to_handler(mocker):
+    """Request's responders should forward requests to correct handlers."""
+    handlers = {method: mocker.Mock() for method in HTTP_METHODS}
+    resp = mocker.Mock()
+    req = mocker.Mock()
+    kwargs = {'arg1': 12, 'arg2': 'someargument'}
+    resource = Resource(handlers, '/some/path/')
+    for method in HTTP_METHODS:
+        getattr(resource, 'on_' + method.lower())(req, resp, **kwargs)
+        handlers[method].handle_request.assert_called_once_with(req, resp, **kwargs)
