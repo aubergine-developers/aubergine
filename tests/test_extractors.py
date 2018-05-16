a58c685fc110fbd4f74df9549afefd517eabdb00
Konrad Jałowiecki <dexter2206@gmail.com>
2018-05-16 12:33:34 +0200

    Add missing test-cases for Extractor

--- tests/test_extractors.py
+++ tests/test_extractors.py
@@ -1,13 +1,24 @@
 """Test cases for content extractors."""
 from falcon import HTTPBadRequest, Request
+from marshmallow import Schema, UnmarshalResult
 import pytest
-from aubergine.extractors import (read_body, read_header, read_path, read_query,
-                                  Location, MissingValueError)
+from aubergine.extractors import (Extractor, read_body, read_header, read_path, read_query,
+                                  Location, MissingValueError, ValidationError)
 
 
 @pytest.fixture(name='http_req')
 def _http_req(mocker):
     return mocker.Mock(spec=Request)
+
+@pytest.fixture(name='schema')
+def _schema(mocker):
+    schema = mocker.Mock(spec=Schema)
+    schema.load.return_value = {'content': {'foo': 'bar'}}, {}
+    return schema
+
+@pytest.fixture(name='decoder')
+def _decoder(mocker):
+    return mocker.Mock()
 
 def test_read_body(http_req):
     body = read_body(http_req)
@@ -52,3 +63,41 @@ def test_raises_missing_query(http_req):
         read_query(http_req, param_name='test')
     assert exc_info.value.location == Location.QUERY
     assert exc_info.value.name == 'test'
+
+def test_raises_missing_required(http_req, schema, decoder, mocker):
+    err = MissingValueError(Location.PATH, 'foo')
+    read_data = mocker.Mock(side_effect=err)
+    ext = Extractor(schema=schema, decoder=decoder, required=True, read_data=read_data)
+    with pytest.raises(MissingValueError) as exc_info:
+        ext.extract(http_req)
+    assert exc_info.value == err
+
+def test_not_raises_optional(http_req, schema, decoder, mocker):
+    read_data = mocker.Mock(side_effect=MissingValueError(Location.QUERY, 'foo'))
+    ext = Extractor(schema=schema, decoder=decoder, required=False, read_data=read_data)
+    result = ext.extract(http_req)
+    assert not result.present
+
+def test_returns_loaded_value(http_req, schema, decoder, mocker):
+    read_data = mocker.Mock()
+    ext = Extractor(schema=schema, decoder=decoder, required=True, read_data=read_data)
+    result = ext.extract(http_req)
+    decoder.decode.assert_called_once_with(read_data.return_value)
+    schema.load.assert_called_once_with({'content': decoder.decode.return_value})
+    assert result.present == True
+    assert result.value == schema.load.return_value[0]
+
+def test_uses_read_data(http_req, schema, decoder, mocker):
+    read_data = mocker.Mock()
+    ext = Extractor(schema=schema, decoder=decoder, required=True, read_data=read_data)
+    ext.extract(http_req, a='foo', x='bar')
+    read_data.assert_called_once_with(http_req, a='foo', x='bar')
+
+def test_raises_validation_error(http_req, decoder, mocker):
+    read_data = mocker.Mock()
+    schema = mocker.Mock()
+    schema.load.return_value = UnmarshalResult({}, [{'test': 'error_msg'}])
+    ext = Extractor(schema=schema, decoder=decoder, required=True, read_data=read_data)
+    with pytest.raises(ValidationError) as exc_info:
+        ext.extract(http_req)
+    assert exc_info.value.errors == [{'test': 'error_msg'}]
